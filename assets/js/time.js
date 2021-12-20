7d7edfed4a092c282a1e183f2c602523dbea4528
linxinh <81026956+linxinghai@users.noreply.github.com>
2021-12-20 22:54:51 +0800

    添加时间功能

---
+++ assets/js/time.js
@@ -0,0 +1,16 @@
+function startTime() {
+    var today = new Date();
+    var hours= today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+    minutes = checkTime(minutes);
+    seconds = checkTime(seconds);
+    document.getElementById('time').innerHTML = hours +":"+minutes+":"+seconds;
+    t =setTimeout(function(){startTime()}, 500);
+}
+function checkTime() {
+    if (i<10) {
+        i="0"+i;
+    }
+    return i;
+}
