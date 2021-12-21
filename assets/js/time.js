e87eb1f6fb5c56c33fc655b6e22945057248cd33
linxinh <81026956+linxinghai@users.noreply.github.com>
2021-12-21 23:32:54 +0800

    修改时间新增天气结构

--- assets/js/time.js
+++ assets/js/time.js
@@ -1,14 +1,20 @@
 function startTime() {
-    var today = new Date();
-    var hours= today.getHours();
-    var minutes = today.getMinutes();
-    var seconds = today.getSeconds();
-    minutes = checkTime(minutes);
-    seconds = checkTime(seconds);
-    document.getElementById('time').innerHTML = hours +":"+minutes+":"+seconds;
-    t =setTimeout(function(){startTime()}, 500);
-    document.getElementById("yeartime").innerHTML = today.toUTCString();
-}
+    Date.prototype.format = function (fmt) {
+        var o = {
+            "y+": this.getFullYear, //年
+            "M+": this.getMonth() + 1, //月份
+            "d+": this.getDate(), //日
+            "h+": this.getHours(), //小时
+            "m+": this.getMinutes(), //分
+            "s+": this.getSeconds() //秒
+        };
+        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+            if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
+    }
+    setInterval("document.getElementById('time').innerHTML = (new Date()).format('yyyy-MM-dd hh:mm:ss');", 1000);
+}    
 function checkTime(i){
     if (i<10) {
         i="0"+i;
