e7856e963202750e203ca966fa0eca2c3a4e3d58
linxinh <linxinh@qq.com>
2022-07-08 22:11:08 +0800

    netlify/function/server.js

--- netlify/function/server.js
+++ netlify/function/server.js
@@ -1,9 +1,7 @@
-const express = require("express")
-const server = express()
 const { Client } = require("@notionhq/client")
 const notion = new Client({ auth: "secret_fRo5dOuwVzYTNjcbMCBJ7jsiAVkUJztborNLyAhcqZR" })
-server.use(express.static("public"))
-server.get("/database", async function (request, response) {
+
+exports.handler = async function(event, context) {
   let data = await notion.databases.query({ database_id: "06dc5ca8e18e4bc2bd4dd59aa570b522" })
   data = data.results.map(i => i.properties)
   data = data.map(i => {
@@ -14,10 +12,12 @@ server.get("/database", async function (request, response) {
       tex: i.text.rich_text[0].plain_text,
       url: i.URL.url,
       t: i.ct.created_time,
-      json: i.JSON.rich_text[0].plain_text,
+      jso: i.JSON.rich_text[0].plain_text,
     }
   })
-  response.json(data)
-})
 
-server.listen(8080)
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  };
+}
