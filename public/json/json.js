ad3063a40713f7c7f8c9da16ca93f9886ca09e99
linxinh <linxinh@qq.com>
2022-09-06 13:51:47 +0800

    123

--- public/json/json.js
+++ public/json/json.js
@@ -1,12 +1,19 @@
-function getJson(src,id) {
+function getJson(src,func) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             myObj = JSON.parse(this.responseText);
-            document.getElementById(id).innerHTML = myObj.name;
+            console.log(myObj);
+            func(myObj);
         }
     };
     xmlhttp.open("GET", src, true);
     xmlhttp.send();
 
+}
+
+function footer(myObj) {
+    document.getElementById("emaila").innerHTML = myObj.email;
+    document.getElementById("phones").innerHTML = myObj.phone;
+    document.getElementById("addresss").innerHTML = myObj.address;
 }
