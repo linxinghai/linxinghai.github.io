3fb47b14704fb627e10d585e6dcc777b647a7ee0
linxinh <linxinh@qq.com>
2023-01-31 00:38:32 +0800

    11

--- public/assets/js/cookie.js
+++ public/assets/js/cookie.js
@@ -1,24 +1,28 @@
-function setCookie(cname,cvalue,exdays) {
+function setCookie(cname, cvalue, exdays) {
     var d = new Data();
-    d.setTime(d.getTime()+(exdays*24*60*60*1000))
-    var expires = "expires="+d.toGMTString();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000))
+    var expires = "expires=" + d.toGMTString();
     document.cookie = cname + "=" + cvalue + "; " + expires;
 }
 
 function getCookie(cname) {
-    return document.cookie;
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0) { return c.substring(name.length, c.length); }
+
+    }
 }
 
 function checkCookie(params) {
-    var user=getCookie("username");
-    if(user!="")
-    {
-        alert("欢迎 "+user+"再次访问");
-    }else{
-        user=prompt("请输入你的名字:","");
-        if(user!=""&&user!=null)
-        {
-            setCookie("username",user,30);
+    var user = getCookie("username");
+    if (user != "") {
+        alert("欢迎 " + user + "再次访问");
+    } else {
+        user = prompt("请输入你的名字:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 30);
         }
     }
 }
@@ -28,5 +32,5 @@ function deleteCookie(cname) {
 }
 
 function displayCookie(params) {
-    
+
 }
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-int main()
-{
-    printf("helloworld");
+int main(void)
+<%
+    int arr<:5:> = {1, 2, 3, 4, 5};
+    printf("%d\n", 2<:arr:>);
     return 0;
-}
+%>
